7cd5a6ae5541a266c3ddc11b6b24cfe16ca0027b
klkl <kuanglong0312@sina.com>
2022-07-05 17:01:49 +0800

    feat: 添加element

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <router-view></router-view>
+  <div>
+    <div>
+      <router-link to="/mainPage">Page1</router-link>
+      <router-link to="/contentPage">Page2</router-link>
+    </div>
+    <router-view></router-view>
+  </div>
 </template>
 <script setup></script>
 <style></style>
--- src/views/ContentPage.vue
+++ src/views/ContentPage.vue
@@ -1,5 +1,17 @@
 <template>
-  <div></div>
+  <div>
+    <el-table :data="tableData" border stripe>
+      <el-table-column label="id" prop="id"></el-table-column>
+      <el-table-column label="name" prop="name"></el-table-column>
+    </el-table>
+  </div>
 </template>
-<script setup></script>
+<script setup>
+const tableData = [
+  {
+    id: 0,
+    name: 'test',
+  },
+]
+</script>
 <style lang=""></style>
