a1a47a2a5bb521d7689dd44940a70fc9357332a7
klkl <kuanglong0312@sina.com>
2022-07-05 10:47:08 +0800

    feat: vite + vue2.7

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <HelloWorld msg="Hello Vue 3 + Vite" />
+  </div>
+</template>
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue2'
+import legacy from '@vitejs/plugin-legacy'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    legacy({
+      targets: ['defaults', 'not ie < 9']
+    })
+  ]
+})
