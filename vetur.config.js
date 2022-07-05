10c4ac1e93bf2aff04b37a2947c414e34f3bc53d
klkl <kuanglong0312@sina.com>
2022-07-05 10:58:58 +0800

    feat: add config

---
+++ vetur.config.js
@@ -0,0 +1,19 @@
+// vetur.config.js
+module.exports = {
+  settings: {
+    'vetur.completion.autoImport': false,
+    'vetur.experimental.templateInterpolationService': false,
+    'vetur.validation.interpolation': false,
+    'vetur.validation.template': false,
+    'vetur.validation.templateProps': false,
+    'vetur.validation.style': false,
+    'vetur.validation.script': false,
+    'vetur.format.enable': false,
+    'vetur.ignoreProjectWarning': true,
+    'vetur.languageFeatures.codeActions': false,
+    'vetur.languageFeatures.semanticTokens': false,
+    'vetur.languageFeatures.updateImportOnFileMove': false,
+    'vetur.trace.server': 'off',
+    'vetur.underline.refValue': false,
+  },
+}
